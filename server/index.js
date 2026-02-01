07aad3479d842e5f8b2273eee5b220545668ed11
harshjani44 <developer.harshjani44@gmail.com>
2026-02-01 23:19:52 +0530

    Changed Backend Workflow

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// All Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/whatsapp", whatsappRoutes);
