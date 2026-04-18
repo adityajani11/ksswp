d7a6d3c1b2996fa89b1610ffadfdcc59f3e78428
harshjani44 <developer.harshjani44@gmail.com>
2026-04-18 16:24:12 +0530

    V5.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,6 @@ import Settings from "./pages/Settings";
 import SuperAdminLogin from "./pages/SuperAdmin/SuperAdminLogin";
 import SuperAdminDashboard from "./pages/SuperAdmin/SuperAdminDashboard";
 
-// Main App component with routing
 export default function App() {
   return (
     <BrowserRouter>
--- server/db.js
+++ server/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// Connect to MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
