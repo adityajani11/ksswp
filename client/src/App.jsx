680a8c103d804afa924d30fcc362d41aa82090c6
harshjani44 <developer.harshjani44@gmail.com>
2026-04-18 16:32:21 +0530

    V5.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import Settings from "./pages/Settings";
 import SuperAdminLogin from "./pages/SuperAdmin/SuperAdminLogin";
 import SuperAdminDashboard from "./pages/SuperAdmin/SuperAdminDashboard";
 
+// This is the main App component that sets up routing for the application using React Router.
 export default function App() {
   return (
     <BrowserRouter>
