dbd5e368907a7bfd8db8338ecf626bce9233cee1
harshjani44 <developer.harshjani44@gmail.com>
2026-04-18 16:39:18 +0530

    V5.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import Settings from "./pages/Settings";
 import SuperAdminLogin from "./pages/SuperAdmin/SuperAdminLogin";
 import SuperAdminDashboard from "./pages/SuperAdmin/SuperAdminDashboard";
 
+// Added
 export default function App() {
   return (
     <BrowserRouter>
